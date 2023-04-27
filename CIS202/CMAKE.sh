3d7d7483afde3005337515d1eb6fec33f42f11ee
Jared-Ponce <129228928+Jared-Ponce@users.noreply.github.com>
2023-04-27 22:02:14 +0000

    43

---
+++ CIS202/CMAKE.sh
@@ -0,0 +1,39 @@
+@Jared-Ponce ➜ .../STL/S-03/3.3/3.3a (master) $ mkdir build
+@Jared-Ponce ➜ .../STL/S-03/3.3/3.3a (master) $ cd build
+@Jared-Ponce ➜ .../S-03/3.3/3.3a/build (master) $ cmake ..
+-- The C compiler identification is GNU 9.4.0
+-- The CXX compiler identification is GNU 9.4.0
+-- Check for working C compiler: /usr/bin/cc
+-- Check for working C compiler: /usr/bin/cc -- works
+-- Detecting C compiler ABI info
+-- Detecting C compiler ABI info - done
+-- Detecting C compile features
+-- Detecting C compile features - done
+-- Check for working CXX compiler: /usr/bin/c++
+-- Check for working CXX compiler: /usr/bin/c++ -- works
+-- Detecting CXX compiler ABI info
+-- Detecting CXX compiler ABI info - done
+-- Detecting CXX compile features
+-- Detecting CXX compile features - done
+-- Configuring done
+-- Generating done
+-- Build files have been written to: /workspaces/CodeSpaces/CIS202/CPlusPlus/STL/S-03/3.3/3.3a/build
+@Jared-Ponce ➜ .../S-03/3.3/3.3a/build (master) $ make
+Scanning dependencies of target 3_3a
+[ 50%] Building CXX object CMakeFiles/3_3a.dir/main.cpp.o
+[100%] Linking CXX executable 3_3a
+[100%] Built target 3_3a
+@Jared-Ponce ➜ .../S-03/3.3/3.3a/build (master) $ make clean
+@Jared-Ponce ➜ .../S-03/3.3/3.3a/build (master) $ make
+[ 50%] Building CXX object CMakeFiles/3_3a.dir/main.cpp.o
+[100%] Linking CXX executable 3_3a
+[100%] Built target 3_3a
+@Jared-Ponce ➜ .../S-03/3.3/3.3a/build (master) $ ls
+3_3a  CMakeCache.txt  CMakeFiles  Makefile  cmake_install.cmake
+@Jared-Ponce ➜ .../S-03/3.3/3.3a/build (master) $ ./3_3a 
+7 10 17 
+7 10 17 
+0 1 2 3 4 5 6 
+@Jared-Ponce ➜ .../S-03/3.3/3.3a/build (master) $ 
+
+
