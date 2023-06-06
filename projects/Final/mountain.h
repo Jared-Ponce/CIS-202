040356fa3791084751ea0768b379c095429f9aaf
Jared-Ponce <129228928+Jared-Ponce@users.noreply.github.com>
2023-06-06 21:02:29 +0000

    1.2.6

---
+++ projects/Final/mountain.h
@@ -0,0 +1,23 @@
+#ifndef MOUNTAIN_H
+#include <string>
+
+class Mountain{
+    private:
+    std::string m_name;
+    std::string m_country;
+    int m_elevation;
+    public:
+    Mountain();
+    Mountain(std::string name, std::string country, int elevation);
+
+    std::string getName() const{return m_name;}
+    std::string getCountry()const{return m_country;}
+    int getElevation()const{return m_elevation;}
+
+    void setName(std::string name){m_name = name;}
+    void setCountry(){}
+    void setElevation(){}
+
+    double toMeters(){}
+};
+#endif
